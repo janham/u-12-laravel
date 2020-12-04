67f2eed0000b49fbe98a63a6fdb255b01e2bb897
Hironori Yamada <hironori.yamada@w-heads.com>
2020-12-04 16:37:19 +0900

    add laravel ui & auth

--- project/html/routes/web.php
+++ project/html/routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('contact/index', 'ContactFormController@index');
+
+// REST
+// Route::resource('contacts', 'ContactFormController')->only(['index', 'show']);
